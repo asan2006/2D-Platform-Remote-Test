257f835d8d6620b39b3be8cef71f2f36fec86445
Robin <bin___03@163.com>
2016-12-07 17:03:05 +0800

    Add files via upload

---
+++ RemoteTest.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<head>
+<META CHARSET="UTF-8">
+<title>2D Platform Remote Test</title>
+</head>
+
+<body>
+<div id="PCI8158">
+<div id="LinearMotion">
+
+</div>
+
+<div id="RotationMotion">
+
+</div>
+</div>
+<div id="DAQ2005">
+
+</div>
+
+</body>
