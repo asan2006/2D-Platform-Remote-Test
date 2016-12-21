07b7fe20f98f6812007195090aa6352c4cb4b5cf
Robin <bin___03@163.com>
2016-12-21 18:31:28 +0800

    161221

--- RemoteTest.php
+++ RemoteTest.php
@@ -1,21 +1,96 @@
 <!DOCTYPE HTML>
 <head>
-<META CHARSET="UTF-8">
-<title>2D Platform Remote Test</title>
+	<META CHARSET="UTF-8">
+	<title>2D Platform Remote Test</title>
+	<style type="text/css">
+		
+	</style>
 </head>
 
 <body>
-<div id="PCI8158">
-<div id="LinearMotion">
-
-</div>
-
-<div id="RotationMotion">
-
-</div>
+	<div id="PCI8158">
+		
+		<div id="LinearMotion">
+			<form>
+				<fieldset>
+					<legend>LinearMotion</legend>
+					<table border="0">
+						<tr>
+							<th colspan="2"><input type="checkbox" name="chkLinearMotion">LinearMotion</th>
+						</tr>
+						
+						<tr>
+							<td>Delay Time(s)</td>
+							<td><input type="text" name="delayTime" /></td>
+						</tr>
+						<tr>
+							<td>Acc Time(s)</td>
+							<td><input name="accTime" /></td>
+						</tr>
+						<tr>
+							<td>Dec Time(s)</td>
+							<td><input name="decTime" /></td>
+						</tr>
+						<tr>
+							<td>Max Speed(mm/s)</td>
+							<td><input name="maxSpeed" /></td>
+						</tr>
+						<tr>
+							<td>Unif Time(s)</td>
+							<td><input name="unifTime" /></td>
+						</tr>
+						<tr>
+							<td>Current Speed(s)</td>
+							<td><input name="curSpeed" /></td>
+						</tr>
+					</table>
+				</fieldset>
+			</form>
+		
+	</div>
+	
+	<!--
+		<div id="LinearMotion">
+		<table border="0">
+		<tr>
+		<th colspan="2"><input type="checkbox" name="chkLinearMotion">LinearMotion</th>
+		</tr>
+		
+		<tr>
+		<td>Delay Time(s)</td>
+		<td><input type="text" name="delayTime" /></td>
+		</tr>
+		<tr>
+		<td>Acc Time(s)</td>
+		<td><input name="accTime" /></td>
+		</tr>
+		<tr>
+		<td>Dec Time(s)</td>
+		<td><input name="decTime" /></td>
+		</tr>
+		<tr>
+		<td>Max Speed(mm/s)</td>
+		<td><input name="maxSpeed" /></td>
+		</tr>
+		<tr>
+		<td>Unif Time(s)</td>
+		<td><input name="unifTime" /></td>
+		</tr>
+		<tr>
+		<td>Current Speed(s)</td>
+		<td><input name="curSpeed" /></td>
+		</tr>
+		</table>
+		
+		</div>
+	-->
+	
+	<div id="RotationMotion">
+		
+	</div>
 </div>
 <div id="DAQ2005">
-
+	
 </div>
 
-</body>
+</body>														
