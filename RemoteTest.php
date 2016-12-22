b0e427e91161b5af4135f59358706d1caea6ef5c
Robin <bin___03@163.com>
2016-12-22 23:24:57 +0800

    Add files via upload

--- RemoteTest.php
+++ RemoteTest.php
@@ -1,95 +1,96 @@
 <!DOCTYPE HTML>
-<head>
-	<META CHARSET="UTF-8">
-	<title>2D Platform Remote Test</title>
-	<style type="text/css">
-		
-	</style>
-</head>
-
-<body>
-	<div id="PCI8158">
-		
-		<div id="LinearMotion">
-			<form>
-				<fieldset style="width:400px">
-					<legend>LinearMotion</legend>
-					<table border="0">
-						<tr>
-							<th colspan="2"><input type="checkbox" name="chkLinearMotion">LinearMotion</th>
-						</tr>
-						
-						<tr>
-							<td>Delay Time(s):</td>
-							<td><input type="text" name="delayTime" value="1"/></td>
-						</tr>
-						<tr>
-							<td>Acc Time(s):</td>
-							<td><input name="accTime"  value="0.1"/></td>
-						</tr>
-						<tr>
-							<td>Dec Time(s):</td>
-							<td><input name="decTime"  value="0.2"/></td>
-						</tr>
-						<tr>
-							<td>Max Speed(mm/s):</td>
-							<td><input name="maxSpeed"  value="60"/></td>
-						</tr>
-						<tr>
-							<td>Unif Time(s):</td>
-							<td><input name="unifTime"  value="0.2"/></td>
-						</tr>
-						<tr>
-							<td>Current Speed(mm/s):</td>
-							<td><input name="curSpeed" placeholder="ADLINK feedback"/></td>
-						</tr>
-					</table>
-				</fieldset>
-			</form>
+<html>
+	<head>
+		<META CHARSET="UTF-8">
+		<title>2D Platform Remote Test</title>
+		<style type="text/css">
 			
+		</style>
+	</head>
+	
+	<body>
+		<div id="PCI8158">
+			
+			<div id="LinearMotion">
+				<form>
+					<fieldset style="width:400px">
+						<legend>LinearMotion</legend>
+						<table border="0">
+							<tr>
+								<th colspan="2"><input type="checkbox" name="chkLinearMotion">LinearMotion</th>
+							</tr>
+							
+							<tr>
+								<td>Delay Time(s):</td>
+								<td><input type="text" name="delayTime" value="1"/></td>
+							</tr>
+							<tr>
+								<td>Acc Time(s):</td>
+								<td><input name="accTime"  value="0.1"/></td>
+							</tr>
+							<tr>
+								<td>Dec Time(s):</td>
+								<td><input name="decTime"  value="0.2"/></td>
+							</tr>
+							<tr>
+								<td>Max Speed(mm/s):</td>
+								<td><input name="maxSpeed"  value="60"/></td>
+							</tr>
+							<tr>
+								<td>Unif Time(s):</td>
+								<td><input name="unifTime"  value="0.2"/></td>
+							</tr>
+							<tr>
+								<td>Current Speed(mm/s):</td>
+								<td><input name="curSpeed" placeholder="ADLINK feedback"/></td>
+							</tr>
+						</table>
+					</fieldset>
+				</form>
+				
+			</div>
+			
+			
+			<div id="RotationMotion">
+				<form>
+					<fieldset style="width:400px">
+						<legend>RotationMotion</legend>
+						<table border="0">
+							<tr>
+								<th colspan="2"><input type="checkbox" name="chkRotMotion">RotationMotion</th>
+							</tr>
+							
+							<tr>
+								<td>Delay Time(s):</td>
+								<td><input type="text" name="delayTime" value="0"/></td>
+							</tr>
+							<tr>
+								<td>Acc Time(s):</td>
+								<td><input name="accTime" value="0.1"/></td>
+							</tr>
+							<tr>
+								<td>Dec Time(s):</td>
+								<td><input name="decTime" value="0.1"/></td>
+							</tr>
+							<tr>
+								<td>Max Speed(deg/s):</td>
+								<td><input name="maxSpeed" value="60"/></td>
+							</tr>
+							<tr>
+								<td>Distance(deg):</td>
+								<td><input name="unifTime" value="60"/></td>
+							</tr>
+							<tr>
+								<td>Current Speed(deg/s):</td>
+								<td><input name="curSpeed" placeholder="ADLINK feedback"/></td>
+							</tr>
+						</table>
+					</fieldset>
+				</form>
+			</div>
 		</div>
-		
-		
-		<div id="RotationMotion">
+		<div id="DAQ2005">
 			<form>
-				<fieldset style="width:400px">
-					<legend>RotationMotion</legend>
-					<table border="0">
-						<tr>
-							<th colspan="2"><input type="checkbox" name="chkRotMotion">RotationMotion</th>
-						</tr>
-						
-						<tr>
-							<td>Delay Time(s):</td>
-							<td><input type="text" name="delayTime" value="0"/></td>
-						</tr>
-						<tr>
-							<td>Acc Time(s):</td>
-							<td><input name="accTime" value="0.1"/></td>
-						</tr>
-						<tr>
-							<td>Dec Time(s):</td>
-							<td><input name="decTime" value="0.1"/></td>
-						</tr>
-						<tr>
-							<td>Max Speed(deg/s):</td>
-							<td><input name="maxSpeed" value="60"/></td>
-						</tr>
-						<tr>
-							<td>Distance(deg):</td>
-							<td><input name="unifTime" value="60"/></td>
-						</tr>
-						<tr>
-							<td>Current Speed(deg/s):</td>
-							<td><input name="curSpeed" placeholder="ADLINK feedback"/></td>
-						</tr>
-					</table>
-				</fieldset>
-			</form>
-		</div>
-	</div>
-	<div id="DAQ2005">
-		<form>
 				<fieldset style="width:400px">
 					<legend>LinearMotion</legend>
 					<table border="0">
@@ -120,6 +121,20 @@
 					</table>
 				</fieldset>
 			</form>
-	</div>
-	
-</body>														
+		</div>
+		
+		<div id="button">
+			<form method="post" action="start.php">
+				<input type="button" value="Start">
+			</form>
+			
+			<form method="post" action="stop.php">
+				<input type="button" value="Stop">
+			</form>
+		</div>
+	</body>		
+</html>
+
+<?php
+	function start() {echo "start";};
+?>
